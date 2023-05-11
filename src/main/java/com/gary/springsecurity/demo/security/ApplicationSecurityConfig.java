59fb8369147bdf4153d8106e9bc9ddcca93ec43a
Gary Ma <gary.ma@entelect.co.za>
2023-05-11 16:59:13 +0200

    CSRF explained

--- src/main/java/com/gary/springsecurity/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/gary/springsecurity/demo/security/ApplicationSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.gary.springsecurity.demo.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.gary.springsecurity.demo.security.ApplicationUserRole.*;
@@ -35,7 +36,9 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
          */
 
         http
-                .csrf().disable()
+                .csrf().disable()   //  Only for non-browser usage!!!
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) //Check how CSRF token was generated
+//                .and()
                 .authorizeRequests()
                 .antMatchers("/","/index", "/css/*","/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
